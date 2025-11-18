b9dba6e6772d49b653e813b761a32a6e59044e17
canhaktan <canhaktanunat@gmail.com>
2025-11-19 00:54:56 +0300

    Add basic /hello endpoint without database

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Azure midterm web app is running");
+        app.MapGet("/", () => "Azure midterm web app is running.");
+
+        app.MapGet("/hello", () => "Hello from /hello endpoint (no database yet).");
 
         app.Run();
     }
