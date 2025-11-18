2cb037da257dab4cfdcd72df6884feb42728565e
canhaktan <canhaktanunat@gmail.com>
2025-11-19 00:44:00 +0300

    Initial base .NET app without /hello

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace AzureMidtermProject;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Azure midterm web app is running");
+
+        app.Run();
+    }
+}
